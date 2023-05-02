cb7827903fa09a5ac8c5d5b64303c8d416656882
Timilehin Oladunni <timilehinayomipo@yahoo.com>
2023-05-02 09:21:29 +0100

    Add project files.

---
+++ Controllers/ApplicationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using WebApiLibrary.Models.ApiRequestDto;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]")]
+    public class ApplicationController : Controller
+    {
+        private readonly ILogger<ApplicationController> logger;
+        public ApplicationController(ILogger<ApplicationController> logger)
+        {
+            this.logger = logger;
+        }
+
+
+        [HttpPost, Route("AddProgramDetails")]
+        public async Task<IActionResult> AddProgramDetails([FromBody] ProgramDetailsModel model)
+        {
+
+        }
+    }
+}
